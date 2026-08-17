ce975fc36c6464d0e20236c23257cf4062e29867
pawkedzia <pawel@radlab.dev>
2026-08-17 18:25:27 +0200

    Remove `pli` info from news container display in `ui_utils_public.py`.

--- streamlit_ui/src/ui_utils_public.py
+++ streamlit_ui/src/ui_utils_public.py
@@ -783,8 +783,12 @@ def add_news_to_public_news_stream(
                 convert_pli_value_to_icon(pli_value)
             )
 
+        # news_container.write(
+        #     f"Info: `3c:`{ico_to_write_p_3c} `pli:`{ico_to_write_pli}"
+        # )
+
         news_container.write(
-            f"Info: `3c:`{ico_to_write_p_3c} `pli:`{ico_to_write_pli}"
+            f"Info: `3c:`{ico_to_write_p_3c}"
         )
 
         news_container.write(user_news_text)
